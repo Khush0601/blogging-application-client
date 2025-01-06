4c813ff7d2cd32fa35f3e540cebe0085515bc8d7
khushboo kumari <ksingh200601@gmail.com>
2025-01-07 00:15:11 +0530

    register page layout completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      
+        screens: {
+          'xs': '480px', // Extra small devices
+          'sm': '640px', // Small devices
+          'md': '768px', // Medium devices
+          'lg': '1024px', // Large devices
+          'xl': '1280px', // Extra large devices
+          '2xl': '1536px', // Custom extra large
+        },
+    },
   },
   plugins: [],
 }
