0548badf43e559c4fa2689a4fb0b4db8d7f25081
khushboo kumari <ksingh200601@gmail.com>
2025-02-01 22:48:54 +0530

    modified home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div>
     <AppHeader headerTitle={'Welcome to Our Blog'}
-      headerDesc={`Start your blog today and join a community of writers and readers who are passionate about sharing their stories and ideas.
+      headerDesc={`Start your blog today and  join a community of writers and readers who are passionate about sharing their stories and ideas.
         We offer everything you need to get started,from helpful tips and tutorials.`}
         learnmore={'Learn more --->'}
     />
