5f4f28f15e6aa550be243ec334d81d5789a3e1bd
khushboo kumari <ksingh200601@gmail.com>
2025-06-21 02:25:41 +0530

    signin googlelogin implemented

--- src/Pages/signIn/SignIn.js
+++ src/Pages/signIn/SignIn.js
@@ -4,7 +4,39 @@ import EmailIcon from '@mui/icons-material/Email';
 import KeyIcon from '@mui/icons-material/Key';
 import GoogleIcon from '@mui/icons-material/Google';
 import { Link } from 'react-router'
+import axios from 'axios';
+import { ErrorToast } from '../../utils/toast';
+import { signInWithPopup } from 'firebase/auth';
+import { useFirebaseContext } from '../../Context/Firebase.Context';
+
 const SignIn = () => {
+  const {auth,fbProvider} = useFirebaseContext();
+  const onGoogleLogin=async()=>{
+    try{
+     const googleResponse=await  signInWithPopup(auth,fbProvider)
+     console.log(googleResponse)
+     let googleToken= await googleResponse.user.getIdToken()
+     let data = '';
+  
+    let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'http://localhost:8000/bloggingApplication/api/v1/user/googleLogin',
+    headers: { 
+      'Authorization': `Bearer${googleToken}`
+    },
+    data : data
+  };
+   let registeredData=await axios.request(config)
+  
+  
+    console.log('userlogin',registeredData)
+    }
+    catch(e){
+    ErrorToast(e.message)
+    }
+    }
+    
   return (
     <div className=' w-screen h-screen flex justify-center items-center'>
         <div className='w-full  xs:w-96 m-2.5 p-2.5 flex flex-col items-center justify-center '>
@@ -24,7 +56,7 @@ const SignIn = () => {
       <div className='my-4 text-gray-400'>OR</div>
       <div className='flex gap-2 px-10 items-center bg-black text-white rounded-3xl py-2 my-4 xs:px-16'>
         <div ><GoogleIcon sx={{fontSize:'20px'}}/></div>
-        <div className='text-sm'>Continue with Google</div>
+        <div className='text-sm' onClick={onGoogleLogin}>Continue with Google</div>
       </div>
       <div className='flex text-sm text-gray-400'>
         <div>Don't have  account ?</div>
