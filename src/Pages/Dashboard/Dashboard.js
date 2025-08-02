4a888df8c197766c78153e4a6a267c4bc58b9fca
khushboo kumari <ksingh200601@gmail.com>
2025-08-02 23:47:21 +0530

    updated Dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -6,9 +6,9 @@ import { UserContext } from '../../App';
 const Dashboard = () => {
   const {user}=useContext(UserContext)
   return (
-    <div className="w-full h-full bg-slate-200">
+    <div className="w-full h-full bg-slate-200 ">
       {/* Header */}
-      <div className="flex justify-between px-6 py-4 items-center bg-white shadow">
+      <div className="flex justify-between px-6 py-4  items-center bg-white shadow">
         <div className="text-xl font-bold">Logo</div>
         <div className="w-12 h-12 rounded-full bg-red-100 flex justify-center items-center text-lg font-medium">
           {user?.name?.charAt(0)}
@@ -16,7 +16,7 @@ const Dashboard = () => {
       </div>
 
       {/* Main Layout */}
-     <div className="flex min-h-screen ">
+     <div className="flex min-h-screen sticky ">
        <SideBar />
        <div className="flex-1 p-4">
          <Outlet />
